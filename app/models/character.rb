de2772db570fd9b4bfd21068d5b0d8ecc729a65b
Katie Seggelke <katie.seggelke@gmail.com>
2019-11-04 21:29:08 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,9 @@
 class Character < ActiveRecord::Base
+  has_many :characters 
+  has_many :actors, through: :characters 
+  
+  def say_that_thing_you_say 
+    puts "#{character.name} always says: #{character.catchphrase}"
+  end 
   
 end
