703210e813cfc39763184fb3c57fe64fee46ab2c
Katie Seggelke <katie.seggelke@gmail.com>
2019-11-04 21:20:13 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,8 +1,10 @@
 class CreateActors < ActiveRecord::Migration[5.1]
   
-  create_table :actors do |t|
-    t.string :first_name 
-    t.string :last_name
+  def change 
+    create_table :actors do |t|
+      t.string :first_name 
+      t.string :last_name
+    end 
   end 
   
 end 
