f8acbe887cc49f86b353810abe3e1a970a15b4ea
Katie Seggelke <katie.seggelke@gmail.com>
2019-11-04 21:36:12 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -5,7 +5,7 @@ class Actor < ActiveRecord::Base
   
   def full_name
     binding.pry 
-    puts "#{self.first_name} #{self.last_name}" 
+    "#{self.first_name} #{self.last_name}" 
   end
   
   def list_roles
