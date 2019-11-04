e7d4d25838e041e643269dbfce1756bbb629319b
Katie Seggelke <katie.seggelke@gmail.com>
2019-11-04 21:37:55 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -10,7 +10,7 @@ class Actor < ActiveRecord::Base
   def list_roles
     self.characters.collect do |c|
       binding.pry 
-      "#{c.name} - #{c.show}"
+      "#{c.name} - #{c.show.name}"
     end 
   end 
   
