d74da9c52bc96ee0e98ffe24d88d3a0c9de2cccb
Katie Seggelke <katie.seggelke@gmail.com>
2019-11-04 21:33:45 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,7 +2,7 @@ class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
   
-  def full_name
+  def full_name(actor)
     puts self.first_name self.last_name 
   end
   
